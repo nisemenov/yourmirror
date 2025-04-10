68252eaa15310e47714b01f1574d3bdde529e395
Nikita Semenov <nasemenov726@gmail.com>
2025-04-10 18:40:01 +0300

    feat: init templates

---
+++ users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import include, path
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
+]
