a538b2bfe8682fcab0d5f59d68465bd06ac07d64
Nikita Semenov <nasemenov726@gmail.com>
2025-04-11 19:04:40 +0300

    feat: init WishItem

--- wishitems/apps.py
+++ wishitems/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class WishitemsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'wishitems'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "wishitems"
--- wishitems/models.py
+++ wishitems/models.py
@@ -1,14 +1,18 @@
 import uuid
+from django.contrib.auth import get_user_model
 from django.db import models
 
+User = get_user_model()
+
+
 def get_picture_path(instance, filename):
-    return 'app_data/profiles/user_{0}/{1}'
+    return "app_data/profiles/user_{0}/{1}"
 
 
 class WishItem(models.Model):
-    id = models.UUIDField(
-        primary_key=True, default=uuid.uuid4, unique=True
-    )
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, unique=True)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="wishitems")
 
     link = models.URLField(blank=True)
     title = models.CharField(max_length=255)
