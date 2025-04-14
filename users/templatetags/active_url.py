53202d9ea35d4dfc1ffcb9c64a3bcd4257509361
Nikita Semenov <nasemenov726@gmail.com>
2025-04-14 18:01:23 +0300

    chore: updated footer.html and header.html

--- users/templatetags/active_url.py
+++ users/templatetags/active_url.py
@@ -4,10 +4,10 @@ register = template.Library()
 
 
 @register.simple_tag(takes_context=True)
-def is_active(context, url_name):
+def is_active_url(context, url_name):
     request = context["request"]
     return (
         "text-black"
         if request.resolver_match and request.resolver_match.url_name == url_name
-        else "text-gray-500"
+        else "text-gray-500 hover:text-black transition duration-200 ease-in-out"
     )
