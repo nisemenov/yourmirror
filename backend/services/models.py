9c84149d374af45117017ba6a2d0fa8e58b1696d
Nikita Semenov <nasemenov726@gmail.com>
2025-05-22 10:45:37 +0300

    migr: add wishitem field in RegistrationTokenModel

--- backend/services/models.py
+++ backend/services/models.py
@@ -6,6 +6,11 @@ from django.db import models
 from django.utils import timezone
 from datetime import timedelta
 
+from wishitems.models import WishItemModel
+
+
+REGISTRATION_TOKEN_EXPIRES: int = 24
+
 
 class RegistrationTokenModel(models.Model):
     email = models.EmailField(unique=True)
@@ -13,6 +18,13 @@ class RegistrationTokenModel(models.Model):
     password_hash = models.CharField(max_length=128)
     token = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
     expires_at = models.DateTimeField()
+
+    wishitem = models.ForeignKey(
+        WishItemModel,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
 
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -22,7 +34,9 @@ class RegistrationTokenModel(models.Model):
 
     def save(self, *args: Any, **kwargs: Any) -> None:
         if not self.expires_at:
-            self.expires_at = timezone.now() + timedelta(hours=24)
+            self.expires_at = timezone.now() + timedelta(
+                hours=REGISTRATION_TOKEN_EXPIRES
+            )
         super().save(*args, **kwargs)
 
     class Meta:
